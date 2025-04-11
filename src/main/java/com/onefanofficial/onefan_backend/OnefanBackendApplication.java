362b002bab548b3925ab119152a3987228c51e90
1fanofficial <1fanofficialx@gmail.com>
2025-04-11 19:20:06 +0530

    initial commit

---
+++ src/main/java/com/onefanofficial/onefan_backend/OnefanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.onefanofficial.onefan_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnefanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnefanBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onefanofficial/onefan_backend/OnefanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onefanofficial.onefan_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnefanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
